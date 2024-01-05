637ccec97437b46f10a7ae74ba1c5d44af69bffb
Luis Martí <lmarti@users.noreply.github.com>
2024-01-05 19:52:58 -0300

    clean up

--- symformer/model/runner.py
+++ symformer/model/runner.py
@@ -6,7 +6,6 @@ import numpy as np
 import tensorflow as tf
 from sklearn.metrics import r2_score
 from sympy import sympify
-from dacite import from_dict
 
 from symformer.dataset.tokenizers import GeneralTermTokenizer
 from symformer.dataset.utils import generate_all_possible_ranges
